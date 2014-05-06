63f646f705f4d1593c66c79b681f2205dfd8aec2
code-for-coffee <jtraverjr@gmail.com>
2014-05-05 20:38:54 -0500

    added getters/setters

--- seleniumJavascriptHelpers/Class1.cs
+++ seleniumJavascriptHelpers/Class1.cs
@@ -13,21 +13,79 @@ namespace seleniumJavascriptHelpers
     /// </summary>
     public virtual class jQueryScriptHelper
     {
-        public static IJavaScriptExecutor Scripts(this IWebDriver driver)
-        {
-            return (IJavaScriptExecutor)driver;
+        public virtual bool GetElementValue(string Selector, IWebDriver Driver) {
+            IJavaScriptExecutor js = Driver as IJavaScriptExecutor;
+            try {
+                string title = (string)js.ExecuteScript(getElementValue(Selector));
+            } catch (WebDriverException e) {
+                Console.WriteLine("jQueryScriptHelper error: " + e);
+                return false;
+            }
+            return true;
         }
-    }
-    #endregion
-    #region Private
-    private class jQueryHeleprsPrivates() {
-        private string setElementValue(string Selector) {
+        public virtual bool GetElementDataAttribute(string Selector, string DataAttribute, IWebDriver Driver) {
+            IJavaScriptExecutor js = Driver as IJavaScriptExecutor;
+            try {
+                string title = (string)js.ExecuteScript(getElementDataAttribute(Selector, DataAttribute));
+            } catch (WebDriverException e) {
+                Console.WriteLine("jQueryScriptHelper error: " + e);
+                return false;
+            }
+            return true;
         }
-        private string getElementValue(string Selector, string Value) {
+        public virtual bool SetElementValue(string Selector, string Value, IWebDriver Driver) {
+            IJavaScriptExecutor js = Driver as IJavaScriptExecutor;
+            try {
+                string title = (string)js.ExecuteScript(getElementDataAttribute(Selector, Value));
+            } catch (WebDriverException e) {
+                Console.WriteLine("jQueryScriptHelper error: " + e);
+                return false;
+            }
+            return true;
         }
-    }
+        public virtual bool SetElementDataAttribute(string Selector, string DataAttribute, string Value, IWebDriver Driver) {
+            IJavaScriptExecutor js = Driver as IJavaScriptExecutor;
+            try {
+                string title = (string)js.ExecuteScript(setElementDataAttribute(Selector, DataAttribute, Value));
+            } catch (WebDriverException e) {
+                Console.WriteLine("jQueryScriptHelper error: " + e);
+                return false;
+            }
+            return true;
+        }
 
     #endregion
+
+    #region Private Methods
+    private string getElementValue(string Selector) {
+        string src = "console.log('jQuery Injection: getElementValue [started]')";
+        src += "$('" + Selector + "').val()";
+        src += "console.log('jQuery Injection: getElementValue [completed]')";
+        return src;
+    }
+    private string getElementDataAttribute(string Selector, string DataAttribute)
+    {
+        string src = "console.log('jQuery Injection: getElementDataAttribute [started]')";
+        src += "$('" + Selector + "').data(" + DataAttribute + ")";
+        src += "console.log('jQuery Injection: getElementDataAttribute [completed]')";
+        return src;
+    }
+    private string setElementDataAttribute(string Selector, string Value)
+    {
+        string src = "console.log('jQuery Injection: setElementDataAttribute [started]')";
+        src += "$('" + Selector + "').val(" + Value + ")";
+        src += "console.log('jQuery Injection: setElementDataAttribute [completed]')";
+        return src;
+    }
+    private string setElementDataAttribute(string Selector, string DataAttribute, string Value)
+    {
+        string src = "console.log('jQuery Injection: setElementDataAttribute [started]')";
+        src += "$('" + Selector + "').data(" + DataAttribute + ", " + Value + ")";
+        src += "console.log('jQuery Injection: setElementDataAttribute [completed]')";
+        return src;
+    }
+    #endregion
+    }
 }
 
 
