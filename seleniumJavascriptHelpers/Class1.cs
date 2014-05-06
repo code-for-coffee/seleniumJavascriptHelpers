91250c81d87a0d9ba1da052f4ace48176d19ef5c
code-for-coffee <jtraverjr@gmail.com>
2014-05-05 20:08:19 -0500

    init commit
    Initial commit

---
+++ seleniumJavascriptHelpers/Class1.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seleniumJavascriptHelpers
+{
+    #region jQueryScriptHelper
+    /// <summary>
+    /// Uses jQuery scripts to manipulate the DOM of your website via Selenium.
+    /// </summary>
+    public virtual class jQueryScriptHelper
+    {
+        public static IJavaScriptExecutor Scripts(this IWebDriver driver)
+        {
+            return (IJavaScriptExecutor)driver;
+        }
+    }
+    #endregion
+    #region Private
+    private class jQueryHeleprsPrivates() {
+        private string setElementValue(string Selector) {
+        }
+        private string getElementValue(string Selector, string Value) {
+        }
+    }
+
+    #endregion
+}
+
+
